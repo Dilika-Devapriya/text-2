fa77e2f1886481288e069bdd1c7b9ab06c81cb45
Dilika-Devapriya <dilika.ramsith@gmail.com>
2024-08-20 10:59:27 +0530

    Update Ups.jsx

--- src/Componets/DCP UPS/Ups.jsx
+++ src/Componets/DCP UPS/Ups.jsx
@@ -16,18 +16,18 @@ const initialUPScard = [
   { image: UPS1, cname: "DCP Line Interactive 1000VA / 1200VA", description: "(1 Ph in -1Ph out )", category: "Line Interactive", url: "dcpups/details2/DCPVersion20" },
   { image: UPS1, cname: "DCP Line Interactive 2000VA / 3000VA", description: "(1 Ph in -1Ph out )", category: "Line Interactive", url: "dcpups/details2/DCPVersion20" },
 
-  //Online Single Phase UPS 
-  { image: UPS2, cname: "DCP Power 1KVA / 2KVA / 3KVA", description: "( 1Ph in -1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
-  { image: UPS2, cname: "DCP Power 6KVA / 10KVA", description: "( 1Ph in -1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
-  { image: UPS2, cname: "DCP Power 15KVA / 120KVA", description: "( 1Ph in -1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
+  // Online Single Phase UPS 
+  { image: UPS2, cname: "DCP Power 1KVA / 2KVA / 3KVA", description: "( 1Ph in - 1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
+  { image: UPS2, cname: "DCP Power 6KVA / 10KVA", description: "( 1Ph in - 1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
+  { image: UPS2, cname: "DCP Power 15KVA / 120KVA", description: "( 1Ph in - 1Ph out )", category: "Online(Single Phase)", url: "dcpcctv/details2/DCP-POWER-TOWER" },
 
-   //Online Three  Phase UPS 
+  // Online Three Phase UPS 
   { image: UPS6, cname: "DCP Power 3300 10KVA", description: "( 3 Ph in - 3Ph out )", category: "Online(Three Phase)", url: "dcpcctv/details2/DCP-POWER-3300" },
   { image: UPS6, cname: "DCP Power 3300 15KVA", description: "( 3 Ph in - 3Ph out )", category: "Online(Three Phase)", url: "dcpcctv/details2/DCP-POWER-3300" },
   { image: UPS6, cname: "DCP Power 3300 20KVA", description: "( 3Ph in - 3Ph out )", category: "Online(Three Phase)", url: "dcpcctv/details2/DCP-POWER-3300" },
   { image: UPS4, cname: "DCP Power 3100", description: "( 3 Ph in - 3Ph out )", category: "Online(Three Phase)", url: "dcpcctv/details2/DCP-POWER-3100" },
 
-  //RackMount UPS's
+  // RackMount UPS's
   { image: UPS3, cname: "DCP Power RT 1KVA", description: "( 3 Ph in - 1 Ph out )", category: "Rack Mount UPS", url: "dcpcctv/details2/DCP-POWER-RT" },
   { image: UPS3, cname: "DCP Power RT 2KVA", description: "( 3 Ph in - 1 Ph out )", category: "Rack Mount UPS", url: "dcpcctv/details2/DCP-POWER-RT" },
   { image: UPS3, cname: "DCP Power RT 3KVA", description: "( 3 Ph in - 1 Ph out )", category: "Rack Mount UPS", url: "dcpcctv/details2/DCP-POWER-RT" },
@@ -36,10 +36,10 @@ const initialUPScard = [
   { image: UPS3, cname: "DCP Power 3100RTRT 6KVA", description: "( 3 Ph in - 1 Ph out )", category: "Rack Mount UPS", url: "dcpcctv/details2/DCP-POWER-RT" },
   { image: UPS3, cname: "DCP Power 3100RTRT 10KVA", description: "( 3 Ph in - 1 Ph out )", category: "Rack Mount UPS", url: "dcpcctv/details2/DCP-POWER-RT" },
   
-  //Mini UPS's
+  // Mini UPS's
   { image: UPS7, cname: "DCP - DC MINI UPS", description: "( DCP DC 418-DC UPS )", category: "Mini Ups", url: "dcpcctv/details2/DCP-DCMINI-UPS" },
 
-  //VRLA Batteries
+  // VRLA Batteries
   { image: UPS8, cname: "DCP 12V-7Ah", description: "( DCP 12V/7AH TO 200AH )", category: "VRLA Batteries", url: "dcpcctv/details2/DCP-BATTERY" },
   { image: UPS8, cname: "DCP 12V-9Ah", description: "( DCP 12V/7AH TO 200AH )", category: "VRLA Batteries", url: "dcpcctv/details2/DCP-BATTERY" },
   { image: UPS8, cname: "DCP 12V-12Ah", description: "( DCP 12V/7AH TO 200AH )", category: "VRLA Batteries", url: "dcpcctv/details2/DCP-BATTERY" },
@@ -49,7 +49,7 @@ const initialUPScard = [
 
 const Ups = () => {
   const [selectedFilter, setSelectedFilter] = useState(null);
-  const [UPScard,] = useState(initialUPScard);  // Manage UPScard as state
+  const [UPScard] = useState(initialUPScard);  // Manage UPScard as state
   const [filteredItems, setFilteredItems] = useState(UPScard);
 
   const filters = ["Line Interactive", "Online(Single Phase)", "Online(Three Phase)", "Rack Mount UPS", "Mini Ups", "VRLA Batteries"];
@@ -63,19 +63,19 @@ const Ups = () => {
   };
 
   useEffect(() => {
-    filterItems();
-  }, [selectedFilter, UPScard]);  // Add UPScard as a dependency
+    const filterItems = () => {
+      if (selectedFilter) {
+        const tempItems = UPScard.filter((item) =>
+          item.category === selectedFilter
+        );
+        setFilteredItems(tempItems);
+      } else {
+        setFilteredItems(UPScard);
+      }
+    };
 
-  const filterItems = () => {
-    if (selectedFilter) {
-      const tempItems = UPScard.filter((item) =>
-        item.category === selectedFilter
-      );
-      setFilteredItems(tempItems);
-    } else {
-      setFilteredItems(UPScard);
-    }
-  };
+    filterItems();
+  }, [selectedFilter, UPScard]);  // Include UPScard as a dependency
 
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
 
@@ -101,27 +101,20 @@ const Ups = () => {
         </div>
 
         <motion.div
-          className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 p-4'
+          className='cards-container flex justify-center flex-wrap gap-8 py-8'
           ref={ref}
           initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 50 }}
-          transition={{ duration: 1 }}
+          animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+          transition={{ duration: 0.7, ease: 'easeOut' }}
         >
-          {filteredItems.map((skill) => (
-            <motion.div
-              key={skill.cname}
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: 50 }}
-              transition={{ duration: 0.5 }}
-            >
-              <UPSCard
-                image={skill.image}
-                cname={skill.cname}
-                description={skill.description}
-                onButtonClick={() => handleButtonClick(skill.url)}
-              />
-            </motion.div>
+          {filteredItems.map((item, idx) => (
+            <UPSCard
+              image={item.image}
+              cname={item.cname}
+              description={item.description}
+              onButtonClick={() => handleButtonClick(item.url)}
+              key={`ups-card-${idx}`}
+            />
           ))}
         </motion.div>
       </div>
